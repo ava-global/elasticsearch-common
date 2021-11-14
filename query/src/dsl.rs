8100a8561d85c98c68486ebe6e6cbca4d98952de
Weeruhputt Supsohmboon <xiikjiix321@gmail.com>
2021-11-14 10:39:48 +0700

    feat: add Clausable proc macro

---
+++ query/src/dsl.rs
@@ -0,0 +1,103 @@
+use bigdecimal::BigDecimal;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum QueryClause {
+    Match {
+        field: String,
+        search_val: String,
+    },
+    Range {
+        field: String,
+        gte: BigDecimal,
+        lte: BigDecimal,
+    },
+}
+
+#[cfg(test)]
+mod tests {
+
+    use bigdecimal::BigDecimal;
+    use bigdecimal::FromPrimitive;
+
+    use query_derive::Clauseable;
+
+    use crate::dsl::QueryClause;
+
+    #[derive(Debug, Clone, Copy)]
+    pub struct Range {
+        lower_bound: Option<i32>,
+        upper_bound: Option<i32>,
+    }
+
+    impl From<(String, Range)> for QueryClause {
+        fn from(tup: (String, Range)) -> Self {
+            let field = tup.0;
+            let range = tup.1;
+            let gte = match range.lower_bound {
+                Some(lower_bound) => BigDecimal::from_i32(lower_bound).unwrap(),
+                None => BigDecimal::from_i32(i32::MIN).unwrap(),
+            };
+
+            let lte = match range.upper_bound {
+                Some(upper_bound) => BigDecimal::from_i32(upper_bound).unwrap(),
+                None => BigDecimal::from_i32(i32::MAX).unwrap(),
+            };
+
+            QueryClause::Range { field, gte, lte }
+        }
+    }
+
+    #[derive(Clauseable)]
+    pub struct SearchCriteria {
+        #[search_field("risk_spectrum")]
+        pub fund_info_risk_spectrum: Option<Range>,
+        #[search_field("fund_statistics.return_ytd")]
+        pub fund_statistics_return_ytd: Option<Range>,
+    }
+
+    #[test]
+    fn given_non_none_range_to_clauses_should_return_correct_vec_of_query_clause() {
+        let criteria = SearchCriteria {
+            fund_info_risk_spectrum: Some(Range {
+                lower_bound: Some(1),
+                upper_bound: Some(10),
+            }),
+            fund_statistics_return_ytd: Some(Range {
+                lower_bound: Some(5),
+                upper_bound: Some(6),
+            }),
+        };
+        let clauses: Vec<QueryClause> = criteria.to_clauses();
+        let expected = vec![
+            QueryClause::Range {
+                field: "risk_spectrum".into(),
+                lte: BigDecimal::from_i32(10).unwrap(),
+                gte: BigDecimal::from_i32(1).unwrap(),
+            },
+            QueryClause::Range {
+                field: "fund_statistics.return_ytd".into(),
+                lte: BigDecimal::from_i32(6).unwrap(),
+                gte: BigDecimal::from_i32(5).unwrap(),
+            },
+        ];
+        assert_eq!(expected, clauses,)
+    }
+
+    #[test]
+    fn given_one_none_range_to_clauses_should_return_correct_vec_of_query_clause() {
+        let criteria = SearchCriteria {
+            fund_info_risk_spectrum: None,
+            fund_statistics_return_ytd: Some(Range {
+                lower_bound: Some(5),
+                upper_bound: Some(6),
+            }),
+        };
+        let clauses: Vec<QueryClause> = criteria.to_clauses();
+        let expected = vec![QueryClause::Range {
+            field: "fund_statistics.return_ytd".into(),
+            lte: BigDecimal::from_i32(6).unwrap(),
+            gte: BigDecimal::from_i32(5).unwrap(),
+        }];
+        assert_eq!(expected, clauses,)
+    }
+}
---
+++ query_derive/src/lib.rs
@@ -0,0 +1,80 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{self, parse_macro_input, DataStruct, DeriveInput, Fields, Ident};
+
+#[proc_macro_derive(Clauseable, attributes(search_field))]
+pub fn calusesable_derive(input: TokenStream) -> TokenStream {
+    let DeriveInput { ident, data, .. } = parse_macro_input!(input);
+    match data {
+        syn::Data::Struct(DataStruct { fields, .. }) => {
+            let struct_name = &ident;
+            impl_to_clauses(struct_name, fields)
+        }
+        _ => unimplemented!(),
+    }
+}
+
+fn impl_to_clauses(struct_name: &Ident, fields: Fields) -> TokenStream {
+    const FIELD_ATTR_NAME: &str = "search_field";
+
+    let mut vec_push_expr = vec![];
+
+    for field in fields {
+        if let Some(search_field_attr) = field
+            .attrs
+            .iter()
+            .find(|a| a.path.is_ident(FIELD_ATTR_NAME))
+        {
+            let search_field_value: syn::LitStr = search_field_attr.parse_args().unwrap();
+            let field_ident = &field.ident.unwrap();
+            vec_push_expr.push(quote! {
+
+                if let Some(criteria_value) = self.#field_ident {
+                    clauses.push(QueryClause::from(
+                            (
+                                #search_field_value.into(),
+                                criteria_value
+                            )
+                        )
+                    );
+                }
+
+            })
+        }
+    }
+
+    let impl_block = quote! {
+        impl #struct_name {
+            pub fn to_clauses(&self) -> Vec<QueryClause> {
+                let mut clauses = vec![];
+                #(#vec_push_expr)*
+                clauses
+            }
+        }
+    };
+    impl_block.into()
+}
+
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = parse_macro_input!(input);
+
+    // Build the trait implementation
+    impl_hello_macro(&ast)
+}
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
